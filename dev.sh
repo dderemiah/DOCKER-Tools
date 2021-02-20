9f90400788734d071ef12ebf44deb382008ab8db
danield <daniel.deremiah@indriver.com>
2021-02-19 16:17:30 -0800

    Path to code and ISOs changed

--- dev.sh
+++ dev.sh
@@ -3,7 +3,7 @@
 docker build -t vpackets-tools-wrs . && \
 docker run -dit --name devops_tools -h vpackets-container \
 -v /disk1/ISOS/:/home/danield/lab-images \
--v /home/danield/git_projects/:/home/danield/code \
--v /home/danield/git_projects/DOCKER-Tools/Ansible/Ansible_variables:/home/danield/ansible \
+-v /dpool/git_projects/:/home/danield/code \
+-v /dpool/git_projects/DOCKER-Tools/Ansible/Ansible_variables:/home/danield/ansible \
 vpackets-tools-wrs:latest && \
 docker exec -it --user danield devops_tools tmux new-session -d -s dock
