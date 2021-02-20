edf217c2702f2081d9cf448867eeef69bf96b490
danield <daniel.deremiah@indriver.com>
2021-02-19 16:24:01 -0800

    corrected ISO path

--- dev.sh
+++ dev.sh
@@ -2,7 +2,7 @@
 # docker-compose up --build
 docker build -t vpackets-tools-wrs . && \
 docker run -dit --name devops_tools -h vpackets-container \
--v /disk1/ISOS/:/home/danield/lab-images \
+-v /dpool/ISOs/:/home/danield/lab-images \
 -v /dpool/git_projects/:/home/danield/code \
 -v /dpool/git_projects/DOCKER-Tools/Ansible/Ansible_variables:/home/danield/ansible \
 vpackets-tools-wrs:latest && \
